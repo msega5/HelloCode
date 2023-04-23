4df887cf7acc5eb6b1bf31db3f4e66a52343f7a4
Sergei Efanov <efanv_serg@mail.ru>
2023-04-23 23:03:49 +0300

    Добавил пример 009 IntroArray

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -1,4 +1,4 @@
-﻿int Max(int arg1, int arg2, int arg3);
+﻿int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
     if (arg2 > result) result = arg2;
@@ -12,13 +12,19 @@ int c1 = 39;
 int a2 = 15;
 int b2 = 21;
 int c2 = 39;
-int a3 = 15;
+int a3 = 1547;
 int b3 = 21;
 int c3 = 39;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+Max(a1, b1, c1),
+Max(a2, b2, c2),
+Max(a3, b3, c3));
+
 
 Console.WriteLine(max);

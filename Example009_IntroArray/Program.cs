75b1590df4decc393932845280883e222fef18ed
Sergei Efanov <efanv_serg@mail.ru>
2023-04-23 23:06:04 +0300

    Поправил код

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -6,13 +6,11 @@
     return result;
 }
 //             0   1   2   3   4   5   6   7   8
-int[] array = {11, 21, 31, 41, 51, 61, 17, 18, 19};
-array[0] = 12;
-Console.WriteLine(array[4]);
+int[] array = {11, 211, 31, 41, 51, 61, 17, 18, 19};
 
-int max = Max(
+int result = Max(
     Max(array[0], array[1], array[2]),
     Max(array[3], array[4], array[5]),
-    Max(array[6], array[7], array[8]),
+    Max(array[6], array[7], array[8])
 );
-Console.WriteLine();
+Console.WriteLine(result);
